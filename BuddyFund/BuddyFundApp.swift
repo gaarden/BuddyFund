09f472a9fb2e1f1aabaaebd1a9cd37cfd10353ca
gaarden <dalbella7365@gmail.com>
2023-05-04 11:10:54 +0900

    Initial Commit

---
+++ BuddyFund/BuddyFundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyFundApp.swift
+//  BuddyFund
+//
+//  Created by Jeongwon Moon on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct BuddyFundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
