13b46c6e20cbe1278977fd1616e10afba57bad43
gaarden <dalbella7365@gmail.com>
2023-05-04 11:23:52 +0900

    Struct folder

---
+++ BuddyFund/Source/Application/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  BuddyFund
+//
+//  Created by Jeongwon Moon on 2023/05/04.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
---
+++ BuddyFund/Source/Application/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  BuddyFund
+//
+//  Created by Jeongwon Moon on 2023/05/04.
+//
+
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    let rootView = Home()
+    
+    if let windowScene = scene as? UIWindowScene {
+      let window = UIWindow(windowScene: windowScene)
+      window.rootViewController = UIHostingController(rootView: rootView)
+      self.window = window
+      window.makeKeyAndVisible()
+    }
+  }
+}
