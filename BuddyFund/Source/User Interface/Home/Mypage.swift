afbc953b1bf1808f658cbc0023a542407868501a
KimMinyeong09 <minyeong9070@naver.com>
2023-06-17 23:13:39 +0900

    [feat] 참여 펀딩 리스트 보기 기능

--- BuddyFund/Source/User Interface/Home/Mypage.swift
+++ BuddyFund/Source/User Interface/Home/Mypage.swift
@@ -74,7 +74,7 @@ private extension Mypage {
     
     var myFundList : some View{
         MyFundListView()
-            .environmentObject(CreateFundListVIewModel(uid: "0cOa7C63F7uJHbAF7qcw"))
+//            .environmentObject(CreateFundListVIewModel(uid: "0cOa7C63F7uJHbAF7qcw"))
     }
 }
 
